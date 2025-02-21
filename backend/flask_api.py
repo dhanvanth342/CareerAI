1e8aa9baa28108292be4709dccdf124a81e8b40f
varsh2001 <varshithchandramukhi1@gmail.com>
2025-02-21 09:30:40 -0600

    made app to deployment mode

--- backend/flask_api.py
+++ backend/flask_api.py
@@ -169,4 +169,4 @@ def road_map():
 if __name__ == '__main__':
     # Use the PORT environment variable for dynamic port binding
     port = int(os.environ.get('PORT', 5000))  # Default to 5000 if not set
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
