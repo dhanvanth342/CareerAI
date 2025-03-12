eeff96fea9762986989cacd57e441384af0b60fb
SatyaJaidev <satyajaidev21@gmail.com>
2025-03-12 12:37:42 -0500

    Changes done on 12-03-25

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import { Box, Typography, Grid, Card, CardMedia, CardContent } from '@mui/material';
 import NavBar from '../components/Navbar';
@@ -105,3 +106,60 @@ const HowItWorks = () => {
 };
 
 export default HowItWorks;
+*/
+
+
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import NavBar from '../components/Navbar';
+import '../components/Styles/About.css';
+import box1 from '../assets/box1.jpg';
+import b2 from '../assets/box2.jpg';
+import b3 from '../assets/box3.jpg';
+import b4 from '../assets/box4.jpg';
+import b5 from '../assets/box5.jpg';
+
+const About = () => {
+  return (
+    <Box className="about-container">
+      <NavBar />
+      <Box className="content-wrapper">
+        <Box className="big-box-1" style={{ backgroundImage: `url(${box1})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}/>
+        <Typography component="div" className="text-content">
+          <h1>STEP 1</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In saepe, consequuntur quod minus amet quas aliquid corrupti optio nisi corporis sequi eveniet id officiis maxime. Nisi laudantium quam provident cumque?</p>
+        </Typography>
+      </Box>
+      <Box className="content-wrapper">
+        <Typography component="div" className="text-content">
+          <h1>STEP 2</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione harum maiores eaque dolores quis provident voluptatibus qui, fuga ducimus commodi blanditiis doloribus praesentium itaque non accusamus ullam! Optio, enim voluptas.\</p>
+        </Typography>
+        <Box className="big-box-2" style={{ backgroundImage: `url(${b2})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}/>
+      </Box>
+      <Box className="content-wrapper">
+        <Box className="big-box-3" style={{ backgroundImage: `url(${b3})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}/>
+        <Typography component="div" className="text-content">
+          <h1>STEP 3</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maxime earum quos in molestias pariatur minima exercitationem eos? Autem dolorum quis illo assumenda qui expedita at nobis ducimus vitae doloremque!</p>
+        </Typography>
+      </Box>
+      <Box className="content-wrapper">
+        <Typography component="div" className="text-content">
+          <h1>STEP 4</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident illo sit optio iure! Esse, eum, dolore maxime adipisci eius nihil, illum vero eaque aut in modi cupiditate nostrum quo.</p>
+        </Typography>
+        <Box className="big-box-4" style={{ backgroundImage: `url(${b4})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}/>
+      </Box>
+      <Box className="content-wrapper">
+        <Box className="big-box-5" style={{ backgroundImage: `url(${b5})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}/>
+        <Typography component="div" className="text-content">
+          <h1>STEP 5</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nam ipsam omnis debitis amet vel harum, unde nostrum beatae rem nihil animi, tempore quos placeat autem sit optio laudantium neque!</p>
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default About;
