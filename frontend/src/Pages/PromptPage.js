1e97b8c326d81980ad67a25a348dc194d85d13e1
varsh2001 <varshithchandramukhi1@gmail.com>
2025-03-22 00:42:42 -0500

    added render routing

--- frontend/src/Pages/PromptPage.js
+++ frontend/src/Pages/PromptPage.js
@@ -48,7 +48,7 @@ const PromptPage = ({ onQuestionsGenerated = () => {} }) => {
     const requestData = { initial_context: text, highest_education: question1, country: question2 };
 
     try {
-      const response = await fetch('http://localhost:5000/generate-questions', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/generate-questions`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(requestData),
